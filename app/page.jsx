9fdb138da36165fe7663405e0a24a481f204ff45
Max Mackie <maxmackie13@gmail.com>
2024-02-18 16:43:06 +0000

    creating boiler plate comonent files and created the desktop navbar

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,5 @@
+import Feed from "@components/Feed";
+
 const Home = () => {
   return (
     <section
@@ -13,7 +15,7 @@ const Home = () => {
         Promptopia is an open-source AI prompting tool for modern world to
         discover, create and share creative prompts
       </p>
-      {/* Feed */}
+      <Feed />
     </section>
   );
 };
